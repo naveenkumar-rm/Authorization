e55d8d41fce51948927836037dad9fc7f41c676e
naveenkumar-rm <naveenkmayilsamy@gmail.com>
2025-10-28 15:09:44 +0530

    New file

--- src/main/java/com/example/authorization/AuthorizationApplication.java
+++ src/main/java/com/example/authorization/AuthorizationApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuthorizationApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(AuthorizationApplication.class, args);
-        System.out.println("🚀 Authorization app is running successfully!");
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationApplication.class, args);
+	}
+
 }
--- src/main/java/com/example/authorization/Repo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.jwtdemo.repo;
-
-import com.example.jwtdemo.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Integer> {
-    Users findByEmail(String email);
-}
